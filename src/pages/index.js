8c0035ece156d722d535916bc6afa0f920fc4f03
Pablo <pablo.kalyell.441@gmail.com>
2023-03-31 11:00:22 -0300

    removing lines extras

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,16 +21,13 @@ import {
   DivNumberCVV,
   DivCVVBack,
   Swapper,
-  FlipCard,
 } from "./styles";
+
+import { useState } from "react";
 
 import visa from "../assets/visa.png";
 import security from "../assets/security.png";
 import payment from "../assets/payment.png";
-import { useState } from "react";
-import InputMask from "react-input-mask";
-
-
 
 function Home() {
 
@@ -41,24 +38,19 @@ function Home() {
 
   const handleChangeName = (event) => {
     setName(event.target.value);
-    
   };
   
   const handleChangeNumber = (event) => {
     setNumber(event.target.value);
-    
   };
 
   const handleChangeValidate = (event) => {
     setValidate(event.target.value);
-    
   };
 
   const handleChangeCode = (event) => {
     setCode(event.target.value);
-    
   };
-
 
   return (
     <Container>
@@ -69,17 +61,19 @@ function Home() {
             <DivNumberCard>
               <label htmlFor="number">Número do cartão</label>
               <InputCard placeholder="Número do cartão" mask="9999 9999 9999 9999" id="number" name="number" value={number} onChange={handleChangeNumber}  disableUnderline/>
-              
             </DivNumberCard>
+
             <DivNick>
               <label htmlFor="name">Nome do titular</label>
               <InputCard  placeholder="Nome como está no cartão" id="name" name="name" type="text" value={name} onChange={handleChangeName} maxLength={20} disableUnderline/>
             </DivNick>
+
             <DivTwoInputs>
               <DivValidate>
                 <label htmlFor="validate">Validade</label>
                 <InputValitade placeholder="mm/aa" mask="99/99" id="validate" name="validate" value={validate} onChange={handleChangeValidate} disableUnderline/>
               </DivValidate>
+
               <DivCVV>
                 <label htmlFor="code">CVV</label>
                 <InputCVV placeholder="***" mask="999" id="code" name="code" value={code} onChange={handleChangeCode} disableUnderline/>
@@ -93,13 +87,14 @@ function Home() {
                 <img src={visa} alt="" />
                 <img className="payment" src={payment} alt="" />
               </DivVisa>
+
               <DivNumbers>
               <span>{number === "" ? <span>4716 9038 02** ****</span> : number} </span>
               </DivNumbers>
+
               <DivNickAndValidate>
                 <span>{name === "" ? <span>Seu nome aqui</span> : name} </span>
                 <span>{validate === "" ? <span>xx/xx</span> : validate} </span>
-                
               </DivNickAndValidate>
             </CardFront>
 
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+
 import bgblur from "../assets/background-blur.png";
 import bgbackblur from "../assets/backgroundback-blur.png";
+
 import InputMask from "react-input-mask";
 
 export const Container = styled.div`
@@ -13,27 +15,30 @@ export const Container = styled.div`
 export const SwapperContainer = styled.div`
   width: 736px;
   height: 420px;
+  padding: 32px;
+
   border: 1px solid #374151;
   background: #1f2937;
   border-radius: 8px;
-  padding: 32px;
+
   @media (max-width: 780px) {
    width: 100%;
    height: 100%;
    padding: 32px 32px;
-   
   }
+
   button {
-    transition: all 0.3s;
     height: 56px;
     width: 100%;
     margin-top: 48px;
+
     color: #fff;
     background: #9333ea;
     box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
     border-radius: 4px;
     font-size: 18px;
-
+    
+    transition: all 0.3s;
     :hover{
       background: #A855F7;
     }
@@ -45,6 +50,7 @@ export const DivInfo = styled.div`
   flex-direction: column;
   width: 50%;
   margin-right: 64px;
+
   @media (max-width: 780px) {
     width:100%;
     margin-right:0px;
@@ -54,208 +60,224 @@ export const DivInfo = styled.div`
 
 export const Swapper = styled.div`
   display: flex;
+
   @media (max-width: 780px) {
-flex-direction: column-reverse;
+    flex-direction: column-reverse;
   }
 `;
-export const FlipCard = styled.div``;
 
 export const DivNumberCard = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 24px;
+
   label {
+    margin-bottom: 4px;
     color: #e5e7eb;
     font-size: 14px;
     line-height: 16px;
     font-weight: 600;
-    margin-bottom: 4px;
   }
 `;
 
 export const InputCard = styled(InputMask)`
   padding: 12px;
-  color: #f3f4f6;
-  outline: none;
   width: 328px;
   height: 48px;
-  :focus {
-    border: 1.5px solid #9333ea;
-  }
+
+  color: #f3f4f6;
   background: #111827;
   border: 1px solid #374151;
   border-radius: 4px;
+
+  outline: none;
+  :focus {
+    border: 1.5px solid #9333ea;
+  }
 `;
 
 export const DivNick = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 24px;
+
   label {
+    margin-bottom: 4px;
     color: #e5e7eb;
     font-size: 14px;
     line-height: 16px;
     font-weight: 600;
-    margin-bottom: 4px;
   }
 `;
 
 export const DivTwoInputs = styled.div`
   display: flex;
   gap: 16px;
-
 `;
 
 export const DivValidate = styled.div`
   display: flex;
   flex-direction: column;
+
   label {
+    margin-bottom: 4px;
     color: #e5e7eb;
     font-size: 14px;
     line-height: 16px;
     font-weight: 600;
-    margin-bottom: 4px;
   }
 `;
 
 export const InputValitade = styled(InputMask)`
   padding: 12px;
-  color: #f3f4f6;
-  outline: none;
   width: 182px;
   height: 48px;
+
+  background: #111827;
+  border: 1px solid #374151;
+  border-radius: 4px;
+  color: #f3f4f6;
+  outline: none;
+
   :focus {
     border: 1.5px solid #9333ea;
   }
-
-  background: #111827;
-
-  border: 1px solid #374151;
-  border-radius: 4px;
 `;
 
 export const DivCVV = styled.div`
   display: flex;
   flex-direction: column;
+
   label {
+    margin-bottom: 4px;
     color: #e5e7eb;
     font-size: 14px;
     line-height: 16px;
     font-weight: 600;
-    margin-bottom: 4px;
   }
 `;
 
 export const InputCVV = styled(InputMask)`
   padding: 12px;
-  color: #f3f4f6;
-  outline: none;
   width: 130px;
   height: 48px;
+
+  background: #111827;
+  border: 1px solid #374151;
+  border-radius: 4px;
+  color: #f3f4f6;
+  outline: none;
+
   :focus {
     border: 1.5px solid #9333ea;
   }
-
-  background: #111827;
-
-  border: 1px solid #374151;
-  border-radius: 4px;
 `;
 
 export const DivCard = styled.div`
   width: 50%;
   position: relative;
+
   transition: transform 0.6s;
   transform-style: preserve-3d;
-  cursor: pointer;
-  @media (max-width: 780px) {
-width: 100%;
-  }
   perspective: 1000px;
+  cursor: pointer;
+
+  @media (max-width: 780px) {
+    width: 100%;
+  }
+
   :hover {
+  transform: rotateY(180deg);
     .info {
       transform: rotateY(-180deg);
     }
-    transform: rotateY(180deg);
   }
 `;
 
 export const CardFront = styled.div`
   width: 280px;
   height: 168px;
-  background: rgba(17, 24, 39, 0.4);
-  backdrop-filter: blur(24px);
-  background-image: url(${bgblur});
-  /* border: 1px solid #000000; */
-  border: none;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  border-radius: 8px;
-  background-position: contain;
   padding: 0px 24px;
   position: absolute;
+
+  background: rgba(17, 24, 39, 0.4);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  border: none;
+
+  background-image: url(${bgblur});
+  backdrop-filter: blur(24px);
+  background-position: contain;
+
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 `;
 
 export const CardBack = styled.div`
-  position: absolute;
   height: 168px;
   width: 280px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  background: rgba(17, 24, 39, 0.4);
-  backdrop-filter: blur(24px);
-  background-image: url(${bgbackblur});
-  /* border: 1px solid #000000; */
-  border: none;
-  border-radius: 8px;
-  background-position: contain;
   padding-top: 16px;
+  position: absolute;
+  left: 0px;
+
+  background: rgba(17, 24, 39, 0.4);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  border: none;
+
+  background-image: url(${bgbackblur});
+  backdrop-filter: blur(24px);
+  background-position: contain;
+
+  transform: rotateY(180deg);
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
-  transform: rotateY(180deg);
-  left: 0px;
-  /* padding:0px 24px; */
 `;
 
 export const Div = styled.div`
-  background: #111827;
   height: 32px;
   width: 100%;
+  background: #111827;
 `;
 
 export const DivCVVBack = styled.div`
-  padding: 0px 24px;
   display: flex;
-  gap: 6px;
   align-items: center;
   justify-content: center;
+
+  padding: 0px 24px;
+  gap: 6px;
   margin-top: 46px;
+
   p {
     color: #fff;
   }
 `;
 
 export const DivNumberCVV = styled.div`
-  background-color: #fff;
   width: 201px;
   height: 32px;
-  border-radius: 4px;
   display: flex;
   justify-content: flex-end;
   align-items: center;
   padding: 12px;
+
+  background-color: #fff;
+  border-radius: 4px;
   color: #000;
 `;
 
 export const DivVisa = styled.div`
-  margin-bottom: 50px;
   display: flex;
+  margin-bottom: 50px;
   justify-content: space-between;
+
   img {
-    margin-top: 16px;
     width: 32px;
     height: 32px;
+    margin-top: 16px;
   }
+
   .payment {
     height: 24px;
     width: 24px;
@@ -263,37 +285,39 @@ export const DivVisa = styled.div`
 `;
 
 export const DivNumbers = styled.div`
+  width: 100%;
   margin-bottom: 24px;
+
   font-weight: 600;
   font-size: 16px;
   line-height: 16px;
+
   text-align:justify;
   letter-spacing:4px;
-  width: 100%;
   color: #fff;
-  span {
-  }
 `;
 
 export const DivNickAndValidate = styled.div`
-  margin-bottom: 24px;
   display: flex;
   justify-content: space-between;
+  margin-bottom: 24px;
+
   span {
     font-weight: 400;
     font-size: 14px;
     line-height: 16px;
-
     color: #f9fafb;
   }
 `;
 
 export const DivSecurityInfo = styled.div`
-  margin-top: 220px;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  margin-top: 220px;
   gap: 8px;
+
   font-size: 14px;
   line-height: 16px;
   color: #e5e7eb;
